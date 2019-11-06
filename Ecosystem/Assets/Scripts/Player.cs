c3a7d026c89b6e9aad7646648bdb1bdbced42927
kayloshai <kayloshai@gmail.com>
2019-11-06 00:09:08 +0000

    Push
    Initial upload.

---
+++ Ecosystem/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //Playter movement input
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	}
+}
