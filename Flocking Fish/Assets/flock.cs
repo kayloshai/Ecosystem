9af1aba5d1ce4abdfdd5877a5c330fc8ced7911e
kayloshai <kayloshai@gmail.com>
2020-01-02 20:15:44 +0000

    Removing of Old project
    Changed from ecosystem to flocking fish and making use of the flocking ai algorithm technique.

---
+++ Flocking Fish/Assets/flock.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flock : MonoBehaviour
+{
+    public float speed = 0.001f;
+    float rotationSpeed = 4.0f;//This is how fast the fish will turn when they need to turn
+    Vector3 averageHeading;//This is the average heading of the group
+    Vector3 averagePosition;//This is the average position of the group
+    float neighbourDistance = 3.0f;//This is the maximum distance they need to be to flock if they are more than this they wont take notice of each other
+    
+    void Start()
+    {
+        speed = Random.Range(0.5f, 1);//Runs the algorithm 1 in 5 times randomly 
+    }
+    void Update()
+    {
+        if(Random.Range(0, 5) < 1)
+            ApplyRules();
+
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;//gos = gameobjects
+        gos = globalFlock.allFish;//The public static allfish array in globalflock script, this is here now so that each fish knows where all other fish are
+
+        Vector3 vcentre = Vector3.zero;//Calculate the centre of the group
+        Vector3 vavoid = Vector3.zero;//Avoidance vector for fish not to hit each other while trying to get to center of the group
+        float gSpeed = 0.1f;//Group Speed
+
+        Vector3 goalPos = globalFlock.goalPos; 
+
+        float dist;
+
+        int groupSize = 0;
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if(dist <= neighbourDistance)//Group size is based on which fish are within the neighbour distance
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if(dist < 1.0f)//If the fish get in a small distance they should avoid
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    flock anotherFlock = go.GetComponent<flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if(groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if(direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                     Quaternion.LookRotation(direction),
+                                     rotationSpeed * Time.deltaTime);
+            
+        }
+    }
+}
---
+++ Flocking Fish/Assets/globalFlock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlock : MonoBehaviour
+{
+    //prefab for fish
+    public GameObject fishPrefab;
+    public int goalPosResetFrequency = 10000;
+    public static int tankSize = 5; //public and static to be accessed outside the script
+    static int numberOfFish = 20;
+    public static GameObject[] allFish = new GameObject[numberOfFish];// made static to be accesible by other scripts
+    public static Vector3 goalPos = Vector3.zero;
+
+    void Start()
+    {
+        for(int i = 0; i <numberOfFish; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0, goalPosResetFrequency) < 50)
+        {
+            goalPos = new Vector3(Random.Range(-tankSize, tankSize),
+                                  Random.Range(-tankSize, tankSize),
+                                  Random.Range(-tankSize, tankSize));
+        }
+    }
+}
